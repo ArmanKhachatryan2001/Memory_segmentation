18acfdd77e4c0f2ecf65a8b843b105b27151487e
Arman <armkhachatryan453@gmail.com>
2023-01-13 18:11:48 +0400

    new branch

--- memory_segmentation.cpp
+++ memory_segmentation.cpp
@@ -10,16 +10,31 @@
 #include "address_adjuster.h"
 #include "memory_prototype.h"
 
-int main() {
-    int i = 0;
-    std::ifstream file;
-    file.open("User_code.cpp");
-    std::string str = "";
-    while (!file.eof()) {
-        std::getline(file, str);
-        if (str != "" && str[0] != '#' && str[0] != '/') {
-            code[i++] = str;
+int main(int argc, char** argv) {
+
+    if (argc == 2) {
+        int i = 0;
+        std::ifstream file;
+        file.open(argv[1]);
+        if (file.is_open()) {
+            std::string str = "";
+            while (!file.eof()) {
+                std::getline(file, str);
+                if (str != "" && str[0] != '#' && str[0] != '/') {
+                    code[i++] = str;
+                }
+            }
+            ran_code(code, i);
+        } else {
+            std::cout << "Error opening file" << '\n';
         }
+    } else {
+        std::cout << "\n\n";
+        std::cout << "Try this way" << '\n';
+        std::cout << "Compile your program." << '\n';
+        std::cout << "g++ user_file_name.cpp" << '\n';
+        std::cout << "g++ memory_segmentation.cpp" << '\n';
+        std::cout << "./a.out user_file_name.cpp" << "\n\n\n";
     }
-    ran_code(code, i);
+    return 0;
 }
